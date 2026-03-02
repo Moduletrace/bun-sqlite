310f14dd756633609bf34fafba343b7abeef9809
Benjamin Toby <devops@tben.me>
2026-03-02 14:58:14 +0100

    Resolve typescript errors

--- dist/utils/grab-db-dir.js
+++ dist/utils/grab-db-dir.js
@@ -1,12 +1,14 @@
 import path from "path";
 import grabDirNames from "../data/grab-dir-names";
+import { AppData } from "../data/app-data";
 export default function grabDBDir({ config }) {
     const { ROOT_DIR } = grabDirNames();
     let db_dir = ROOT_DIR;
     if (config.db_dir) {
         db_dir = config.db_dir;
     }
-    const backup_dir = path.resolve(db_dir, config.db_backup_dir);
+    const backup_dir_name = config.db_backup_dir || AppData["DefaultBackupDirName"];
+    const backup_dir = path.resolve(db_dir, backup_dir_name);
     const db_file_path = path.resolve(db_dir, config.db_name);
     return { db_dir, backup_dir, db_file_path };
 }
--- src/utils/grab-db-dir.ts
+++ src/utils/grab-db-dir.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import grabDirNames from "../data/grab-dir-names";
 import type { BunSQLiteConfig } from "../types";
+import { AppData } from "../data/app-data";
 
 type Params = {
     config: BunSQLiteConfig;
@@ -15,7 +16,10 @@ export default function grabDBDir({ config }: Params) {
         db_dir = config.db_dir;
     }
 
-    const backup_dir = path.resolve(db_dir, config.db_backup_dir);
+    const backup_dir_name =
+        config.db_backup_dir || AppData["DefaultBackupDirName"];
+
+    const backup_dir = path.resolve(db_dir, backup_dir_name);
     const db_file_path = path.resolve(db_dir, config.db_name);
 
     return { db_dir, backup_dir, db_file_path };
